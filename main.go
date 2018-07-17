df890454796133d1bbaa6157f90ece2a38d54801
Sören Sprößig <ssproessig@googlemail.com>
2018-07-17 23:21:56 +0200

    CF integration: take port from env "PORT" and default to 8080

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"github.com/gorilla/mux"
 )
 
 
 func main() {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+	port = ":" + port
+	log.Printf("Listening on port %s", port)
+
 	router := mux.NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(port, router))
 }
